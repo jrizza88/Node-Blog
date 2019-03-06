da2c334b00e6d9d1476e52716ccb2fa18459f187
jrizza88 <jetorres88@gmail.com>
2019-03-05 21:26:33 -0500

    specific post id successful, non-existing id message works and error successful

--- data/routes/postRoutes.js
+++ data/routes/postRoutes.js
@@ -9,15 +9,29 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        console.log('query', req.query)
         const posts = await postDb.get(req.query)
         res.status(200).json(posts)
     } 
-    catch (err) {
-        console.error(err)
+    catch (error) {
+        console.error(error)
         res.status(500).json({error: "The posts could not be rendered"})
     }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const posts = await postDb.getById(req.params.id)
+        if (posts) {
+            res.status(200).json(posts)
+        } else {
+            res.status(404).send(`<h1>The post you searched for does not exist</h1>`)
+        }
+    } catch {
+        res.status(500).json({error: 'id resource not found'})
+    }
 })
+
+
 
 
 module.exports = router;
